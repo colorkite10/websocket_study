925c7bb53c4935cba645a297f713d9f3ea6249f4
colorkite10 <colorkite10@gmail.com>
2024-07-11 16:09:28 +0900

    feat: WebSocket Events로 서버와 브라우저 소통하기

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,3 @@
-alert("hi");
+//브라우저와 서버를 연결해주는 코드
+//fSocket: 서버로의 연결
+const fSocket = new WebSocket(`ws://${window.location.host}`);
--- src/server.js
+++ src/server.js
@@ -7,8 +7,8 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
@@ -16,5 +16,12 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 //http 서버 위에 webSocket 서버를 만들 수 있도록 한 것
 // => http 서버와 wss 서버 모두 지원 가능하게 함
+
+function handleConnection(bSocket) {
+  //bSocket: 연결된 브라우저
+  console.log(bSocket);
+}
+
+wss.on("connection", handleConnection);
 
 server.listen(3000, handleListen);
