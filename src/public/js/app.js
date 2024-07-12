902a1c086f2f9a41e080a998d0d944d9df3bf716
colorkite10 <colorkite10@gmail.com>
2024-07-12 13:12:02 +0900

    feat: socketio로 서버와 브라우저 소통하기

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,16 @@
 const socket = io();
+
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+
+function handleRoomSubmit(event) {
+  event.preventDefault();
+  const input = form.querySelector("input");
+  //emit(event name, payload want to sended, function executed at the server)
+  socket.emit("enter_room", { payload: input.value }, () => {
+    console.log("server is done!");
+  }); //object를 보낼 수 있음!
+  input.value = "";
+}
+
+form.addEventListener("submit", handleRoomSubmit);
