e1769ff63a98b72a1d63ba9cc7adb25183c283c7
colorkite10 <colorkite10@gmail.com>
2024-07-15 09:16:25 +0900

    feat: 현재 개설된 방 정보 보여주기

--- src/server.js
+++ src/server.js
@@ -16,20 +16,46 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
+function publicRooms() {
+  const { rooms, sids } = wsServer.sockets.adapter;
+  // const {
+  //   sockets: {
+  //     adapter: { sids, rooms },
+  //   },
+  // } = wsServer;
+
+  // const sids=wsServer.sockets.adapter.sids;
+  // const rooms=wsServer.sockets.adapter.rooms;
+
+  const publicRooms = [];
+  rooms.forEach((_, key) => {
+    if (sids.get(key) === undefined) {
+      publicRooms.push(key);
+    }
+  });
+  return publicRooms;
+}
+
 wsServer.on("connection", (socket) => {
   socket["nickname"] = "Anon";
   socket.onAny((event) => {
+    console.log(wsServer.sockets.adapter);
     console.log(`Socket Event: ${event}`);
   });
   socket.on("enter_room", (roomName, done) => {
     socket.join(roomName);
     done();
     socket.to(roomName).emit("welcome", socket.nickname);
+    wsServer.sockets.emit("room_change", publicRooms());
   });
   socket.on("disconnecting", () => {
     socket.rooms.forEach((room) =>
       socket.to(room).emit("bye", socket.nickname)
     );
+    wsServer.sockets.emit("room_change", publicRooms());
+  });
+  socket.on("disconnect", () => {
+    wsServer.sockets.emit("room_change", publicRooms());
   });
   socket.on("new_message", (msg, room, done) => {
     socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
